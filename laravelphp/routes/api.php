1c7eda422f0d0278f42bfc31499cf6651dda595d
Miguel <miguelvargasdemarque@gmail.com>
2023-03-30 21:02:29 -0300

    Criada primeira rota que acessa o banco de dados e lista os usuarios cadastrados no banco

--- laravelphp/routes/api.php
+++ laravelphp/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\SignUpController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -21,3 +22,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::get('/salve-galera', function () {
     return 'Hello World';
 });
+
+Route::get('/teste', [SignUpController::class, "index"])->name("teste");
