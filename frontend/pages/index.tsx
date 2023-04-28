4bd64c223526f37a1f4ac61ff9a3056a702973b7
Randerson Lemos <randerson.lemos@gmail.com>
2023-04-27 21:16:15 -0300

    Requisicao para o chatbot

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Head from 'next/head'
 import styles from '@/styles/Home.module.css'
 import { useTheme } from '@mui/material'
 import Header from '@/components/header/header'
 import SignUpForm from '@/components/signUpForm/signUpForm'
+import axios from 'axios'
 
 type HomeProps = {
   setActiveTheme: React.Dispatch<React.SetStateAction<'light' | 'dark'>>
@@ -11,6 +12,28 @@ type HomeProps = {
 
 export default function Home({ setActiveTheme }: HomeProps) {
   const theme = useTheme()
+  const [data, setData] = useState({
+    number: "5519988352366",
+    message: "Sou um bot arrombado"
+  })
+
+  useEffect(() => {
+    async function sendRequest() {
+      const options = {
+        method: 'POST',
+        url: 'http://mc426chatbot.ddns.net/api/chatbot/sendtext',
+        headers: { 'Content-Type': 'application/json' },
+        data: { number: '5519988352366', message: 'Hello World (Eu sou o chatbot)' }
+      };
+
+      axios.request(options).then(function (response) {
+        console.log(response.data);
+      }).catch(function (error) {
+        console.error(error);
+      });
+    }
+    sendRequest()
+  }, [])
 
   return (
     <>
