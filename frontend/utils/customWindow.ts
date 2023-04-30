b0eaf07796f1308a976e031b3370a978070b6f6f
Miguel <miguelvargasdemarque@gmail.com>
2023-04-30 17:30:30 -0300

    Removendo codigo duplicado

--- frontend/utils/customWindow.ts
+++
@@ -1,6 +0,0 @@
-import { RecaptchaParameters, ConfirmationResult } from 'firebase/auth'
-
-export interface FirebaseWindow extends Window {
-  recaptchaVerifier: RecaptchaParameters
-  confirmationResult: ConfirmationResult
-}
