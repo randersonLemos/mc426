893779c5da258e7646d4cf24f28696b5cc838a04
Danilo Pereira da SIlva <danilopereira_b2@hotmail.com.br>
2023-07-11 03:07:54 -0400

    Fix: Type error: Cannot find module '@components/map'

---
+++ frontend/components/mapDiv/mapDiv.tsx
@@ -0,0 +1,7 @@
+import React, { FormEvent, useState } from "react";
+
+export default function MapDiv() {
+  return (
+    <mapDiv id="map" style={{width:800 + 'px'}, {height: 500 + 'px'}}/>
+  );
+}
