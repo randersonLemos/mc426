9763404dddf122606a4aba7cc25b1b77a4ab0ed1
Danilo Pereira da SIlva <danilopereira_b2@hotmail.com.br>
2023-07-11 01:37:50 -0400

    Added testing for maps integration

---
+++ frontend/cypress/e2e/mapsIntegration/mapsIntegration.cy.tsx
@@ -0,0 +1,10 @@
+describe('Verify Maps Integration', () => {
+
+    it('Verify Maps is visible', () => {
+
+        cy.visit('http://localhost:3000/map');
+
+        cy.get('#map').should('be.visible');
+
+    })
+});
