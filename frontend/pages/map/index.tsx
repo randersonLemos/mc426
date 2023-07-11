0663f6de599a62256837ae1418db7eab2fafe708
Danilo Pereira da SIlva <danilopereira_b2@hotmail.com.br>
2023-07-11 01:45:49 -0400

    Removing unused code, fixing new lines and tab and spaces mixture

--- frontend/pages/map/index.tsx
+++ frontend/pages/map/index.tsx
@@ -4,31 +4,17 @@ import MapDiv from "@components/map";
 import { Loader } from "@googlemaps/js-api-loader";
 import styles from "@/styles/Home.module.css";
 
-
-//import GoogleMapReact from "google-map-react";
-
 class App extends Component {
-	
-
-	
 	render() {  
-	    return (
-		
-
-		  <span>
-		    <script async
-		    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCLXtI7gKepIrTL5IvXt4HCJYJxGxDFCBs
-		    &libraries=maps,marker&v=beta">
-		    </script>
-		    <gmp-map center="-22.9005447311, -47.0543764492"  zoom="10" map-id="map_id"></gmp-map>
-		  </span	>
-		
-
-	    );
-	  }
-	  //<mapDiv id="map" style={{width : 500 + 'px'}, {heigth: 500 + 'px'}}/>
-	  //	<div className={styles.campaignContainer} style={{height : 100 + '%'}, {width : 100 + '%'}}>
-	  //<gmp-map center="37.4220656,-122.0840897" -22.9005447311 -47.0543764492 zoom="10" map-id="map_id"></gmp-map>
+		return (
+			<span>
+				<script async
+				src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCLXtI7gKepIrTL5IvXt4HCJYJxGxDFCBs
+				&libraries=maps,marker&v=beta">
+				</script>
+				<gmp-map id="map" center="-22.9005447311, -47.0543764492"  zoom="10" map-id="map_id"></gmp-map>
+			</span>		
+		);
+	}
 }
-
 export default App
