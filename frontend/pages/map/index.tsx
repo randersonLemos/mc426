649abe198d342367dce3de3f127d20b4ac68843f
Danilo Pereira da SIlva <danilopereira_b2@hotmail.com.br>
2023-07-11 01:04:17 -0400

    Added map to application

--- frontend/pages/map/index.tsx
+++ frontend/pages/map/index.tsx
@@ -2,33 +2,33 @@ import React, { useState, Component } from "react";
 import MapDiv from "@components/map";
 
 import { Loader } from "@googlemaps/js-api-loader";
+import styles from "@/styles/Home.module.css";
 
 
 //import GoogleMapReact from "google-map-react";
 
 class App extends Component {
 	
-	componentDidMount(){
-	    const loader = new Loader({
-		  apiKey: "AIzaSyCLXtI7gKepIrTL5IvXt4HCJYJxGxDFCBs",
-		  version: "weekly",
-		});
 
-		loader.load().then(async () => {
-		  const { Map } = await google.maps.importLibrary("maps");
-
-		  map = new Map(document.getElementById("map"), {
-		    center: { lat: -34.397, lng: 150.644 },
-		    zoom: 8,
-		  });
-		});
-	  }
 	
 	render() {  
 	    return (
-	      <mapDiv id="map" style={{width : 500 + 'px'}, {heigth: 500 + 'px'}}/>
+		
+
+		  <span>
+		    <script async
+		    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCLXtI7gKepIrTL5IvXt4HCJYJxGxDFCBs
+		    &libraries=maps,marker&v=beta">
+		    </script>
+		    <gmp-map center="-22.9005447311, -47.0543764492"  zoom="10" map-id="map_id"></gmp-map>
+		  </span	>
+		
+
 	    );
 	  }
+	  //<mapDiv id="map" style={{width : 500 + 'px'}, {heigth: 500 + 'px'}}/>
+	  //	<div className={styles.campaignContainer} style={{height : 100 + '%'}, {width : 100 + '%'}}>
+	  //<gmp-map center="37.4220656,-122.0840897" -22.9005447311 -47.0543764492 zoom="10" map-id="map_id"></gmp-map>
 }
 
 export default App
