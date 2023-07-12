7c53c7af6a56fa1d4d5939bca77c752c7e1630dd
Danilo Pereira da SIlva <danilopereira_b2@hotmail.com.br>
2023-07-11 21:37:19 -0400

    Trying to make map work

--- frontend/pages/map/index.tsx
+++ frontend/pages/map/index.tsx
@@ -3,17 +3,59 @@ import React, { useState, Component } from "react";
 import styles from "@/styles/Home.module.css";
 import { Loader } from "@googlemaps/js-api-loader"
 
+
+const loader = new Loader({
+  apiKey: "AIzaSyCLXtI7gKepIrTL5IvXt4HCJYJxGxDFCBs",
+  version: "weekly",
+  libraries: ["places"]
+});
+
+const mapOptions = {
+  center: {
+    lat: 0,
+    lng: 0
+  },
+  zoom: 4
+};
+
+
+
+	
+
 class App extends Component {
-	render() {  
+ componentDidMount() {
+	   loader
+	  .importLibrary('maps')
+	  .then(({Map}) => {
+	    new Map(document.getElementById("map"), mapOptions);
+	  })
+	  .catch((e) => {
+	    // do something
+	  });
+	  
+	  loader
+	  .load()
+	  .then((google) => {
+	    new google.maps.Map(document.getElementById("map"), mapOptions);
+	  })
+	  .catch(e => {
+	    // do something
+	  });
+   return
+ }
+
+ render() {
 		return (
-			<span>
+			
 				<script async
 				src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCLXtI7gKepIrTL5IvXt4HCJYJxGxDFCBs
 				&libraries=maps,marker&v=beta">
 				</script>
-				<gmp-map id="map" center="-22.9005447311, -47.0543764492"  zoom="10" map-id="map_id"></gmp-map>
-			</span>		
+				,<div id="map" ></div>
+				
 		);
 	}
+	
 }
+  
 export default App
